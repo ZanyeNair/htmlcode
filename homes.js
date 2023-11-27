f6710e74c81745135817b2c967183863c97aed18
ZanyeNair <zayne.nair@circuitrunners.com>
2023-11-26 23:54:34 -0500

    yay

--- homes.js
+++ homes.js
@@ -1,2 +1,25 @@
 function printHi()
-{document.write("Hello from homes.js");}
+    {
+        document.write("Hello from homes.js");
+    }
+
+// Path: homes.js
+
+function maths(){
+    var a = 10;
+    var b = 20;
+    var c = a + b;
+    document.write("The sum of a and b is: " + c);
+}
+
+function getName(){
+    let firstName, lastName, fullName;
+
+    firstName = prompt("Enter your first name: ");
+    lastName = prompt("Enter your last name: ");
+
+    fullName = firstName + " " + lastName;
+
+    document.write("Hello " + fullName);
+}
+
